3276d937091848832c33a7f332ebf13e3bd536e9
j3dzski <j3dzski@hotmail.com>
2018-08-17 14:29:50 +0800

    Modified templates to display post from database.

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+"""  
+	This is a newly created url pattern to be over write the 
+existing and main Django default web page. In other words this url pattern will be called at jedzsite
+url.py or imported to suite our custom web page
+
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('', views.post_list, name='post_list'),		
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+# --------------------------------------------------------------------------------------------
+# Here we are saying that Django go to the same directory ("." denotes) and look into def Post 
+# and get usefull instruction from there
+from .models import Post
+#---------------------------------------------------------------------------------------------
+from django.utils import timezone
 
 # Create your views here.
+def post_list(request):
+	#------- order the Posts according to date it was published and store it to variable "posts" as QuerySet
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	#------- something the template (in this case the post_list.html) can use 
+	return render(request, 'blog/post_list.html', {'posts': posts})
+		
