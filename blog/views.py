8c153d0021f42e859beff091d37458e1b31b532d
j3dzski <j3dzski@hotmail.com>
2018-08-29 15:08:06 +0800

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,12 @@ from django.shortcuts import render, get_object_or_404
 # and get usefull instruction from there
 from .models import Post
 #---------------------------------------------------------------------------------------------
+# just want to get all the library of time zone for python
 from django.utils import timezone
+#---------------------------------------------------------------------------------------------
+# Add this import inorder to let the add form and edit form to work
+from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):
@@ -16,3 +21,35 @@ def post_list(request):
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post': post})
+
+#---- Code below is for new blog onced clicked the + button
+	
+def post_new(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})
+
+#---- Code below is for editing routines in the blog once clicked the pencil button
+
+def post_edit(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method =="POST":
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm(instance=post)
+	return render(request, 'blog/post_edit.html', {'form': form})
+	
